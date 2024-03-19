6a68b07ae449fa4454f466a7e4b64c461645527a
RaphaelPaul21 <148201361+RaphaelPaul21@users.noreply.github.com>
2024-03-19 23:53:36 +0100

    push code

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import MemeGenerator from './components/MemeGenerator';
+import './components/MemeGenerator_css.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MemeGenerator />
+      </header>
+    </div>
+  );
+}
+
+export default App;
